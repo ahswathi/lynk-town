603ee667e4304a25f81be05ef61d5cdd61faa04b
swathi <swathiashok94@gmail.com>
2022-07-18 17:24:14 +0530

    Accordion

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,11 @@ let Header = () => {
 
     return (
         <>
-            <div className="header-wrapper  ">
-                <div className="max-h-[210px] lg:max-h-[724px] sm:max-h-[420px] bg-[#6A5B40]">
+            <div className="header-wrapper mb-[140px] sm:mb-[180px] md:mb-[180px]  ">
+                <div className="h-[280px] lg:h-[724px] md:h-[540px] sm:h-[480px] bg-[#6A5B40]">
                     <p className="logo text-white lg:text-[33px] sm:text-[26px] text-[16px] ml-[30px]  pt-2 sm:ml-20 sm:pt-2 ">Logo</p>
                     <div className="header">
-                        <p className="title pt-2 lg:text-[52px] sm:text-[32px] text-[12px] text-center text-white">Create an order
+                        <p className="title pt-4 lg:text-[52px] sm:text-[32px] text-[12px] text-center text-white">Create an order
                             <span className="sub-title lg:text-[52px] sm:text-[32px] text-[12px] text-center
                              text-[#6A5B40] rounded-full bg-[#EAE7E0] px-1 mx-1"> Lynk and share it</span><br />
 
@@ -21,14 +21,14 @@ let Header = () => {
                     </svg> */}
 
 
-                    <div className="flex justify-around mt-[40px] z-0 sm:mt-[60px] md:mt-[60px]   sm:mt-[60px] lg:mt-[80px] mx-[50px] sm:mx-[100px] lg:mx-[120px] gap-8 sm:gap-14 lg:gap-34">
-                        <div className="mt-[20px] sm:mt-[60px] sm:mt-[80px]">
+                    <div className="flex justify-around mt-[40px] z-0  sm:mt-[60px] md:mt-[60px] lg:mt-[80px] mx-[50px] sm:mx-[100px] lg:mx-[120px] gap-8 sm:gap-14 lg:gap-34">
+                        <div className="mt-[40px] sm:mt-[60px] ">
                             <img src="/images/mobile1.png" className="w-full" alt="service1" />
                         </div>
-                        <div className="mobile2-img">
+                        <div className="mobile2-img mt-[20px]">
                             <img src="/images/mobile2.png" alt="service1" />
                         </div>
-                        <div className="mt-[20px] sm:mt-[60px] sm:mt-[80px]">
+                        <div className="mt-[40px] sm:mt-[60px] ">
                             <img src="/images/mobile3.png" alt="service1" />
                         </div>
                     </div>
